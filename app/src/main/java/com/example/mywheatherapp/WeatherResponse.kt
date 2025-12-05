4e46fc1e5cb9ae68539198ee89cf319e175dab2e
Vaibhav Malviya <vaibhavmalviya0017@gmail.com>
2025-12-05 11:29:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mywheatherapp/WeatherResponse.kt
@@ -0,0 +1,21 @@
+package com.example.mywheatherapp
+
+
+data class WeatherResponse(
+
+    val name: String,
+    val main: Main,
+    val weather: List<Weather>
+
+)
+
+data class Main(
+
+    val temp : Float,
+    val humidity : Int
+)
+
+data class Weather(
+
+    val description:String
+)
